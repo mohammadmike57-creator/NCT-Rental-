78364aa73e882cfa85e9ac502983cbec42c382d4
Cayenne <cayenne@Cayennes-MacBook-Air.local>
2026-03-25 18:02:44 +0400

    Add ClearIcon to icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -336,3 +336,9 @@ export const CodeBracketIcon: React.FC<{ className?: string }> = ({ className = 
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
   </svg>
 );
+
+export const ClearIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+  </svg>
+);
